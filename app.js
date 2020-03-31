fee5ebb6c4b6e65aa3a44044a8838fa9df00d750
unknown <gaffney339911@gmail.com>
2020-03-31 16:55:13 -0500

    app.js db.sequelize

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 // express
 const express = require('express');
 const app = express();
+db = require('./db')
 
 // controllers
 const user = require('./controllers/usercontroller');
@@ -10,8 +11,8 @@ const myMemes = require('./controllers/myMemesController');
 const feed = require('./controllers/feedController');
 
 // import db
-const sequelize = require('./db');
-sequelize.sync(); // {force: true} 
+// const sequelize = require('./db');
+// sequelize.sync(); // {force: true} 
 app.use(express.json());
 
 // middleware
@@ -25,4 +26,9 @@ app.use('/mymemes', myMemes);
 
 // protected routes
 
-app.listen(process.env.PORT, () => console.log(`app has become self aware on port ${process.env.PORT}`));
+
+db.sequelize.sync().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`app has become self aware on port ${process.env.PORT}`);
+    });
+});
