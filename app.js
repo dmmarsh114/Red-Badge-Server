00c40f1f9cd985bcd3fde955ec9c12a5c6dcd192
unknown <gaffney339911@gmail.com>
2020-04-01 10:04:30 -0500

    minor changes after db object change

--- app.js
+++ app.js
@@ -3,22 +3,20 @@ require('dotenv').config();
 // express
 const express = require('express');
 const app = express();
-db = require('./db')
+const db = require('./db');
+const headers = require('./middleware/headers');
 
 // controllers
 const user = require('./controllers/usercontroller');
 const myMemes = require('./controllers/myMemesController');
 const feed = require('./controllers/feedController');
-<<<<<<< HEAD
-const myMemes = require('./controllers/myMemes');
 const vote = require('./controllers/votingController');
-=======
->>>>>>> a277fe98b5fe5b5614e425e6ee9731bf79eadb09
 
 // import db
 // const sequelize = require('./db');
 // sequelize.sync(); // {force: true} 
 app.use(express.json());
+app.use(headers);
 
 // middleware
 // app.use(require('./middleware/headers'));
