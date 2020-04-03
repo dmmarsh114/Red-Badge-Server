2896d0f39625be783be611e7fe225577f2e15849
unknown <gaffney339911@gmail.com>
2020-04-02 20:14:31 -0500

    changes to models

--- db.js
+++ db.js
@@ -15,14 +15,14 @@ db.sequelize = sequelize;
 
 //models
 db.user = require('./models/user')(sequelize, Sequelize);
-db.memes = require('./models/memes')(sequelize, Sequelize);
-db.comments = require('./models/comments')(sequelize, Sequelize);
+db.meme = require('./models/memes')(sequelize, Sequelize);
+db.comment = require('./models/comments')(sequelize, Sequelize);
 
 //db relations
-db.user.hasMany(db.memes);
-db.memes.belongsTo(db.user);
-db.memes.hasMany(db.comments);
-db.comments.belongsTo(db.memes);
+db.user.hasMany(db.meme);
+db.meme.belongsTo(db.user);
+db.meme.hasMany(db.comment);
+db.comment.belongsTo(db.meme);
 
 // console.log(db);
 
